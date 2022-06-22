0075558a0d68838728ae747735a3229537621490
nguyencobap <nguyennh@bkav.com>
2022-06-22 08:47:08 +0700

    first commit

---
+++ BACThread.cpp
@@ -0,0 +1,80 @@
+#include "BACThread.h"
+
+BACThread::BACThread(const char* threadName)
+    : m_destroyThread(false)
+{
+    THREAD_NAME = threadName;
+}
+
+BACThread::~BACThread() {
+    Quit();
+}
+
+/**
+ * Create thread
+ * @param   void
+ * */
+int32_t BACThread::Start() {
+    return pthread_create(&m_threadID, NULL, &BACThread::Process, this);
+}
+
+/**
+ * Post runnable vào looper để thực thi.
+ * @param   funcPtr:    con trỏ hàm muốn thực thi
+ * @param   data:       data cần thiết cho funcPtr
+ * */
+void BACThread::Post(FuncPtr funcPtr, void* data)
+{
+    Runnable runnable(funcPtr, data);
+
+    m_queue.push(runnable);
+    pthread_cond_signal(&m_cv);
+}
+
+/**
+ * Looper thực hiện xử lý runnable post bởi BACThread::Post
+ * @param   This    BACThread Pointer
+ * */
+void* BACThread::Process(void *This) {
+    BACThread* pBACThread = (BACThread*) This;
+    while (!pBACThread->m_destroyThread)
+    {
+        while (pBACThread->m_queue.empty())
+            pthread_cond_wait(&pBACThread->m_cv, &pBACThread->m_mutex);
+
+        Runnable runnable = pBACThread->m_queue.front();
+        pBACThread->m_queue.pop();
+
+        if (runnable.funcPtr && runnable.data)
+        {
+            runnable.funcPtr(runnable.data);
+        }
+    }
+    pthread_exit(NULL);
+    pthread_cond_destroy(&pBACThread->m_cv);
+    pthread_mutex_destroy(&pBACThread->m_mutex);
+}
+
+/**
+ * Abort thread ngay lập tức
+ * @param   void
+ * */
+void BACThread::Quit() {
+    m_destroyThread = true;
+}
+
+/**
+ * Thực thi các Runnable chưa chạy xong rồi mới abort thread
+ * @param   void
+ * */
+void BACThread::QuitSafely() {
+
+    FuncPtr f = [](void* This){
+        BACThread* pBACThread = (BACThread*) This;
+        pBACThread->m_destroyThread = true;
+    };
+
+    Runnable runnable(f, this);
+    m_queue.push(runnable);
+}
+
---
+++ BACThread.h
@@ -0,0 +1,65 @@
+#include <stdint.h>
+#include <queue>
+#include <pthread.h>
+
+typedef void (*FuncPtr)(void*);
+
+class Runnable
+{
+public:
+    FuncPtr funcPtr;
+    void* data;
+    Runnable(FuncPtr funcPtr, void* data) {
+        this->funcPtr = funcPtr;
+        this->data = data;
+    };
+    ~Runnable() {
+    };
+};
+
+class BACThread
+{
+public:
+    BACThread(const char* threadName);
+    ~BACThread();
+    /**
+     * Create thread
+     * @param   void
+     * */
+    int32_t Start();
+    /**
+     * Abort thread ngay lập tức
+     * @param   void
+     * */
+    void Quit();
+    /**
+     * Thực thi các Runnable chưa chạy xong rồi mới abort thread
+     * @param   void
+     * */
+    void QuitSafely();
+    /**
+     * Post runnable vào looper để thực thi.
+     * @param   funcPtr:    con trỏ hàm muốn thực thi
+     * @param   data:       data cần thiết cho funcPtr
+     * */
+    void Post(FuncPtr funcPtr, void* data);
+    /**
+     * Lấy thread id
+     * */
+    int32_t GetThreadID() {
+        return m_threadID;
+    };
+private:
+    /**
+     * Looper thực hiện xử lý runnable post bởi BACThread::Post
+     * @param   This    BACThread Pointer
+     * */
+    static void* Process(void *This);
+
+    std::queue<Runnable> m_queue;
+    const char* THREAD_NAME;
+    pthread_t m_threadID;
+    bool m_destroyThread = false;
+    pthread_cond_t m_cv = PTHREAD_COND_INITIALIZER;
+    pthread_mutex_t m_mutex = PTHREAD_MUTEX_INITIALIZER;
+};
